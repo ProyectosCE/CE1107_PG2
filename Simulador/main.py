fcc58937c353d2840ac8125a7e626cbe4d195851
JBB092 <jos.barquero@estudiantec.cr>
2025-06-11 22:51:53 -0600

    [ADD] Creación de prueba real
    Se creó un módulo de prueba real con las demás etapas del procesador

--- Simulador/main.py
+++ Simulador/main.py
@@ -1,24 +1,16 @@
-# main.py
-
 from core.processor import Processor
 
-print("\n SIMULACIÓN RISC-V: IF → ID → EX \n")
+print("\n SIMULACIÓN RISC-V: IF → ID → EX → MEM \n")
 
 program = [
-    "add x1, x2, x3",
-    "sub x4, x1, x5",
-    "addi x6, x0, 10",
-    "beq x2, x3, 8",
-    "and x7, x1, x2"
+    "addi x2, x0, 4",        # x2 = 4
+    "addi x3, x0, 1234",     # x3 = 1234
+    "sw x3, 0(x2)",          # mem[4] = 1234
+    "lw x1, 0(x2)",          # x1 = mem[4] => 1234
+    "add x5, x1, x3"         # x5 = x1 + x3 => 2468
 ]
-
-initial_regs = {
-    "x2": 10,
-    "x3": 5,
-    "x5": 3
-}
 
 cpu = Processor()
 cpu.load_program(program)
-cpu.preload_registers(initial_regs)
+cpu.preload_registers({})  # se puede setear valores si se quiere
 cpu.run()
