9710053cbe613d2539c76f05ba5ad100d3ee6739
JBB092 <jos.barquero@estudiantec.cr>
2025-06-22 09:26:33 -0600

    [ADD] Referencias de las latencias

--- Simulador/config.py
+++ Simulador/config.py
@@ -4,3 +4,6 @@ LATENCY_ID = 1e-9    # 1ns
 LATENCY_EX = 2e-9    # 2ns
 LATENCY_MEM = 4e-9   # 4ns
 LATENCY_WB = 1e-9    # 1ns
+
+# Harris & Harris - Digital Design and Computer Architecture: RISC-V Edition
+# Computer Organization and Design: The Hardware/Software Interface (Patterson & Hennessy)
